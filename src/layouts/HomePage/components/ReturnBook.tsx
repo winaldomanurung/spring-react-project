0809d8ac1249acb8a26da9168302e67ce16342bf
winaldomanurung <winaldo_satryadi@rocketmail.com>
2023-03-20 11:54:03 +0700

    fetch book from api and add loading spinner

--- src/layouts/HomePage/components/ReturnBook.tsx
+++ src/layouts/HomePage/components/ReturnBook.tsx
@@ -1,16 +1,22 @@
-export const ReturnBook = () => {
+import BookModel from "../../../models/BookModel";
+
+export const ReturnBook: React.FC<{ book: BookModel }> = (props) => {
   return (
     <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
       <div className="text-center">
-        <img
-          src={require("./../../../Images/BooksImages/book2.jpg")}
-          width="151"
-          height="233"
-          alt="book"
-        />
+        {props.book.img ? (
+          <img src={props.book.img} width="151" height="233" alt="book" />
+        ) : (
+          <img
+            src={require("./../../../Images/BooksImages/book2.jpg")}
+            width="151"
+            height="233"
+            alt="book"
+          />
+        )}
 
-        <h6 className="mt-2">Book</h6>
-        <p>Kodepatra</p>
+        <h6 className="mt-2">{props.book.title}</h6>
+        <p>{props.book.author}</p>
         <a className="btn main-color text-white">Reserve</a>
       </div>
     </div>
